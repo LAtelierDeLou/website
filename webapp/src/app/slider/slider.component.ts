8561934f3c74d18be80c062a02d95e2f2339ab7b
JimmyRobz <jimmy@robz.re>
2017-12-12 23:28:15 +0100

    Complete simple carousel with slick.

--- webapp/src/app/slider/slider.component.ts
+++ webapp/src/app/slider/slider.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'slider-component',
     templateUrl: './slider.component.html',
     styleUrls: ['./slider.component.css']
 })
-export class SliderComponent {
+export class SliderComponent implements AfterViewInit {
     title = 'slider';
 
-    constructor() {}
+    @ViewChild('pictures') pictures: ElementRef;
 
+    constructor() {
+    }
+
+    ngAfterViewInit(): void {
+        const pictures: any = $(this.pictures.nativeElement);
+        pictures.slick({
+            fade: true,
+            dots: true,
+            infinite: true,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            pauseOnFocus: false,
+            pauseOnHover: false,
+            pauseOnDotsHover: false
+        });
+    }
 
 }
